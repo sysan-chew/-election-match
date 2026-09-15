bff2724336d7787f72705bbe6beec14f038d8eab
sysan <nsx_love@ezweb.ne.jp>
2026-09-15 14:29:16 +0900

    Create route.js

---
+++ app/api/elections/route.js
@@ -0,0 +1,15 @@
+import data from "../../../../data/elections.json";
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const area = searchParams.get("area");
+
+  const elections = area
+    ? data.elections.filter((election) => election.areaId === area)
+    : [];
+
+  return Response.json({
+    areas: data.areas,
+    elections
+  });
+}
