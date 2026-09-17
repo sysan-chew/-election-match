f9694ace464d24a843d626ac6f7080aebd1fd817
sysan <nsx_love@ezweb.ne.jp>
2026-09-17 15:31:00 +0900

    Update route.js

--- app/api/candidates/route.js
+++ app/api/candidates/route.js
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 export async function GET() {
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
   );
 
   const { data, error } = await supabase
